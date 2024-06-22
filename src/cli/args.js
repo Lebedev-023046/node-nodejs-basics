ec72d1e2bf2ae203f5f3086d532a5e0ec074749d
lebedev-023046 <lebedev.023046@gmail.com>
2024-06-22 14:41:59 +0300

    feat: solve cli block

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const customArgs = process.argv.slice(2)
+    console.log(customArgs)
+
+    for (let i = 0; i < customArgs.length; i += 2) {
+        const argumentName = customArgs[i];
+        const argumentValue = customArgs[i + 1];
+        console.log(`${argumentName} is ${argumentValue}`)
+
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+
+    const envObj = process.env
+
+    const keysWithProperPrefix = Object.keys(envObj).filter(key => key.startsWith('RSS'))
+    const properKeyValuePairs = keysWithProperPrefix.forEach(key => console.log(`${key}=${envObj[key]}`))
 };
 
 parseEnv();
