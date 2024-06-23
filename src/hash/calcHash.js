5b82f96abebea823330cece0caf859220ca848b4
lebedev-023046 <lebedev.023046@gmail.com>
2024-06-23 14:30:43 +0300

    feat: solve hash block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash, randomBytes } from 'crypto';
+import { createReadStream } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256')
+    const readStream = createReadStream(resolve(__dirname, './files/fileToCalculateHashFor.txt'))
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk)
+    })
+
+    readStream.on('end', () => {
+        const hashResult = hash.digest('hex')
+        console.log(hashResult)
+    })
+
 };
 
 await calculateHash();
