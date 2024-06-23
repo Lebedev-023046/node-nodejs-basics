67ef06a3ea8b5f72a167355945b9cbb2ee70e782
lebedev-023046 <lebedev.023046@gmail.com>
2024-06-23 14:18:46 +0300

    feat: solve zip block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const readStream = createReadStream(resolve(__dirname, './files/fileToCompress.txt'))
+const writeStream = createWriteStream(resolve(__dirname, './files/archive.gz'))
+
+const compressStream = zlib.createGzip()
+
 const compress = async () => {
-    // Write your code here 
+    readStream.pipe(compressStream).pipe(writeStream)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,44 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { Transform } from 'node:stream';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+
+class DecompressStream extends Transform {
+
+    constructor () {
+        super()
+        this.decompressedData = []
+    }
+
+    _transform(chunk, encoding, callback) {
+        this.decompressedData.push(chunk)
+        callback()
+    }
+
+    _flush(callback) {
+        const concatenatedChunks = Buffer.concat(this.decompressedData)
+        zlib.unzip(concatenatedChunks, (err, decompressedData) => {
+            if(err) {
+                callback(err)
+                return
+            }
+            this.push(decompressedData)
+            callback()
+        })
+    }
+}
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+
+const readStream = createReadStream(resolve(__dirname, './files/archive.gz'))
+const writeStream = createWriteStream(resolve(__dirname, './files/decompressedData.txt'))
+
 const decompress = async () => {
-    // Write your code here 
+    const decompressStream = new DecompressStream()
+    readStream.pipe(decompressStream).pipe(writeStream)
 };
 
 await decompress();
